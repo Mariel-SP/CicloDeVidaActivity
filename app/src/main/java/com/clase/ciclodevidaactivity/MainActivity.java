d2460973a9f97eadb0c24e63bbb989f7202f1818
Mariel-SP <annia.salcedopi@anahuac.mx>
2022-01-26 18:58:57 -0500

    Initial commit

---
+++ app/src/main/java/com/clase/ciclodevidaactivity/MainActivity.java
@@ -0,0 +1,38 @@
+package com.clase.ciclodevidaactivity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    protected void onStart(){
+        super.onStart();
+    }
+
+    protected void onResume(){
+        super.onResume();
+    }
+
+    protected void onPause(){
+        super.onPause();
+    }
+
+    protected void onStop(){
+        super.onStop();
+    }
+
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
